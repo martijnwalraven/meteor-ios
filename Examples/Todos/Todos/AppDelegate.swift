bff69278e1ea82056039368798fd5ecf21a8f0e9
Martijn Walraven <martijn@martijnwalraven.com>
2016-04-01 14:43:38 +0200

    Change serverURL for Todos example to localhost

--- Examples/Todos/Todos/AppDelegate.swift
+++ Examples/Todos/Todos/AppDelegate.swift
@@ -22,7 +22,7 @@ import UIKit
 import CoreData
 import Meteor
 
-let Meteor = METCoreDataDDPClient(serverURL: NSURL(string: "wss://meteor-ios-todos.meteor.com/websocket")!)
+let Meteor = METCoreDataDDPClient(serverURL: NSURL(string: "ws://localhost:3000/websocket")!)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
