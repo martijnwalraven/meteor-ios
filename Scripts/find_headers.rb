6ca1c0dbae8bc155ac0be27b281e48a925267cc5
Martijn Walraven <martijn@martijnwalraven.com>
2016-04-02 08:15:55 +0200

    Replace local return with next in find_headers.rb
    This avoids a LocalJumpError in newer Ruby versions.

--- Scripts/find_headers.rb
+++ Scripts/find_headers.rb
@@ -67,14 +67,14 @@ fail "Could not find target '#{options[:target]}'" unless target
 
 public_headers = target.headers_build_phase.files.select do |build_file|
   settings = build_file.settings
-  return unless settings
+  next unless settings
   attributes = settings['ATTRIBUTES']
   attributes && attributes.include?('Public')
 end
 
 private_headers = target.headers_build_phase.files.select do |build_file|
   settings = build_file.settings
-  return unless settings
+  next unless settings
   attributes = settings['ATTRIBUTES']
   attributes && attributes.include?('Private')
 end
